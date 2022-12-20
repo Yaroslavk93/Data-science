9ccffccd4e6033fb8c191b41b1d448e647451276
Yaroslavk93 <108370875+Yaroslavk93@users.noreply.github.com>
2022-12-20 17:50:27 +0300

    Delete Task_3 directory

--- Task_3/info.py
+++
@@ -1,9 +0,0 @@
-def info(docs, direct):
-    for i in range(len(docs)):
-        for key, value in direct.items():
-            if docs[i]['number'] in value:
-                print(
-                    '№: %s, тип: %s, владелец: %s, полка хранения: %s'
-                    %(docs[i]['number'], docs[i]['type'],
-                      docs[i]['name'], key)
-                )
