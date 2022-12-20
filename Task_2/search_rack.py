e94f08fb0e2244f29b3860dbd42b5168e0845c91
Yaroslavk93 <108370875+Yaroslavk93@users.noreply.github.com>
2022-12-20 17:50:19 +0300

    Delete Task_2 directory

--- Task_2/search_rack.py
+++
@@ -1,8 +0,0 @@
-def search_rack(direct):
-    num = input('Введите номер документа: ')
-    for key, value in direct.items():
-        if num in value:
-            print(f'Документ хранится на полке: {key}')
-            break
-    else:
-        print('Документ не найден в базе.')
