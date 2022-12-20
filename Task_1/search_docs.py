b72a392b8a01527e3f3fb003d9cb0e137e129811
Yaroslavk93 <108370875+Yaroslavk93@users.noreply.github.com>
2022-12-20 17:50:10 +0300

    Delete Task_1 directory

--- Task_1/search_docs.py
+++
@@ -1,8 +0,0 @@
-def search_docs(docs):
-    num = input('Введите номер документа: ')
-    for i in range(len(docs)):
-        if num in docs[i].values():
-            print('Владелец документа: %s' % docs[i]['name'])
-            break
-    else:
-        print('Документ не найден в базе.')
